1778ffd5c666d6ddf246f1273d3efd191d7a2d12
ShivamMishra828 <shivam9878522087@gmail.com>
2024-11-22 12:25:56 +0530

    feat: added success and error response classes

---
+++ src/utils/responses.ts
@@ -0,0 +1,30 @@
+class BaseResponse {
+    success: boolean;
+    message: string;
+
+    constructor(success: boolean, message: string) {
+        this.success = success;
+        this.message = message;
+    }
+}
+
+export class SuccessResponse<T> extends BaseResponse {
+    data?: T;
+
+    constructor(
+        data?: T,
+        message: string = 'Successfully completed the request',
+    ) {
+        super(true, message);
+        this.data = data;
+    }
+}
+
+export class ErrorResponse<T> extends BaseResponse {
+    error?: T;
+
+    constructor(error?: T, message: string = 'Something went wrong') {
+        super(false, message);
+        this.error = error;
+    }
+}
